e2d79ab91cef85da94ac0d1af1ff8718af4a675c
isuru9625 <isurufernando.16@cse.mrt.ac.lk>
2023-12-06 22:59:31 +0530

    build info

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def get_statistics():
         return jsonify({'error': 'Failed to fetch commit information'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0' , port =int("3000") , debug=True)
+    app.run(host='0.0.0.0'  , debug=True)
