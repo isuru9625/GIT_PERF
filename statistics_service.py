0c1a9dacd3cfbc9e4a0394466fdf9520a8c107fb
isuru9625 <isurufernando.16@cse.mrt.ac.lk>
2023-12-07 13:41:15 +0530

    build info

--- statistics_service.py
+++ statistics_service.py
@@ -116,7 +116,7 @@ def insert_metrics_into_mysql(metrics):
     
     try:
         connection = mysql.connector.connect(
-            host='localhost',
+            host='mysql-container',
             database='GIT_PERF',
             user='root',
             password='root'
